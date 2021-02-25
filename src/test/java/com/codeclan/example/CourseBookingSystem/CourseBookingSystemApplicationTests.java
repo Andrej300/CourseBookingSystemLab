b56e9f980d6b9317e422006de697a8a0fbb426f0
jamiemalcolm <jmalcolm281@gmail.com>
2021-02-25 12:15:24 +0000

    models working and can create instances

--- src/test/java/com/codeclan/example/CourseBookingSystem/CourseBookingSystemApplicationTests.java
+++ src/test/java/com/codeclan/example/CourseBookingSystem/CourseBookingSystemApplicationTests.java
@@ -1,13 +1,44 @@
 package com.codeclan.example.CourseBookingSystem;
 
+import com.codeclan.example.CourseBookingSystem.models.Booking;
+import com.codeclan.example.CourseBookingSystem.models.Course;
+import com.codeclan.example.CourseBookingSystem.models.Customer;
+import com.codeclan.example.CourseBookingSystem.repositories.BookingRepository;
+import com.codeclan.example.CourseBookingSystem.repositories.CourseRepository;
+import com.codeclan.example.CourseBookingSystem.repositories.CustomerRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.Table;
 
 @SpringBootTest
 class CourseBookingSystemApplicationTests {
 
+	@Autowired
+	CourseRepository courseRepository;
+
+	@Autowired
+	BookingRepository bookingRepository;
+
+	@Autowired
+	CustomerRepository customerRepository;
+
+
+
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void canCreateBookingCourseAndCustomer(){
+		Booking booking1 = new Booking("25-02-2021");
+		bookingRepository.save(booking1);
+		Course course = new Course("intro to python", "Edinburgh", 5);
+		courseRepository.save(course);
+		Customer customer = new Customer("Andre", "Dunfermline", 29);
+		customerRepository.save(customer);
 	}
 
 }
